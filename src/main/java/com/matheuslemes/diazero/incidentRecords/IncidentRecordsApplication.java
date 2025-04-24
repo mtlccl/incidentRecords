82203927bbbb74a11b54b57f927bba71542293fd
mtlccl <matheus.carvalho.lemes@hotmail.com>
2025-04-24 18:07:03 -0300

    fc-one

---
+++ src/main/java/com/matheuslemes/diazero/incidentRecords/IncidentRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.matheuslemes.diazero.incidentRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuslemes/diazero/incidentRecords/IncidentRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuslemes.diazero.incidentRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
