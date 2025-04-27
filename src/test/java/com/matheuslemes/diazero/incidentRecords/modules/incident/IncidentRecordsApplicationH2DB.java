6d95eddbd093106424a7c74828638dbfc54d2292
mtlccl <matheus.carvalho.lemes@hotmail.com>
2025-04-27 12:47:59 -0300

    h2 tester db

---
+++ src/test/java/com/matheuslemes/diazero/incidentRecords/modules/incident/IncidentRecordsApplicationH2DB.java
@@ -0,0 +1,17 @@
+package com.matheuslemes.diazero.incidentRecords.modules.incident;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+public class IncidentRecordsApplicationH2DB {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentRecordsApplicationH2DB.class, args);
+    }
+
+}
